1b4f8a040c0cfbdaf1541f4d0008e8c2a1ca94bd
G0Z3RO <ibrahim3rifat@gamil.com>
2025-03-02 17:11:31 +0600

    file updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('question-task').addEventListener('click', function () {
-  window.location.href = '../next-index.html';
+  window.location.href = './next-index.html';
 });
 document.getElementById('clear-btn').addEventListener('click', function () {
   const history = document.getElementById('history');
